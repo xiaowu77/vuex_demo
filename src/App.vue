c2edd119951c9e9f90c8c38d5c79a13dc381f01a
xiaowu77 <2678713877@qq.com>
2020-06-09 17:55:01 +0800

    初始化计数器项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <my-addition></my-addition>
+    <p>-------------------------------</p>
+    <my-subtraction></my-subtraction>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Addition from "./components/Addition"
+  import Subtraction from "./components/Subtraction"
+  export default {
+      data(){
+          return {};
+      },
+      components:{
+          "my-addition": Addition,
+          "my-subtraction": Subtraction
+      }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
